35f1c62db73918a625601734856c01a46079c253
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-23 15:52:53 +0200

    fix style in header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,10 @@
 <header>
+    <div class="bg-primary">
+        <div class="container d-flex justify-content-end">
+            <a href="#" class="text-light link-underline-opacity-0 link-underline">DC POWER VISA</a>
+            <a href="#" class="text-light link-underline-opacity-0 link-underline">ADDITIONAL DC SITES</a>
+        </div>
+    </div>
     <div class="container">
         <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
         <ul>
