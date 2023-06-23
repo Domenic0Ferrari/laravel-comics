0faaeb795ffb202359905ecc17d48e430bcb1c8b
Domenic0Ferrari <125200308+Domenic0Ferrari@users.noreply.github.com>
2023-06-23 15:22:32 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+    {{-- qui servono gli // invece dei .. --}}
+</head>
+<body>
+    <h1 class="text-center bg-warning">Ciao da vite!</h1>
+    {{-- <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> --}}
+    {{-- per inserire le immagini si usa questo percorso --}}
+</body>
+</html>
