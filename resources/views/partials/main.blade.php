aad2a80a6d0fdeaf7f50aeb0d699b06800e636bf
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-26 10:17:02 +0200

    add card in main-top

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -2,12 +2,12 @@
     {{-- parte superiore --}}
     <div id="main-top">
         <div class="container">
-            <div class="row">
+            <div class="row pt-5">
                 {{-- ciclare le col-2 per stampare le immagini dei fumetti e il loro titolo --}}
                 @foreach ($comics as $comic)
                 <div class="col-2">
-                    <img src="{{ Vite::asset($comic['thumb']) }}" alt="#">
-                    <h5>{{ $comic['series'] }}</h5>
+                    <img src="{{ Vite::asset($comic['thumb']) }}" alt="#" class="img-comic">
+                    <h5 class="text-light comic-title pb-5 pt-3">{{ $comic['series'] }}</h5>
                 </div>
                 @endforeach
             </div>
