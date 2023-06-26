b67bad0f94d68fe122d0d8f1fec6addf9bdce4a8
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-26 10:07:07 +0200

    add image and title with @foreach in main.blade

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -4,10 +4,12 @@
         <div class="container">
             <div class="row">
                 {{-- ciclare le col-2 per stampare le immagini dei fumetti e il loro titolo --}}
+                @foreach ($comics as $comic)
                 <div class="col-2">
-                    <img src="#" alt="#">
-                    <h5>AGGIUNGERE TITOLO</h5>
+                    <img src="{{ Vite::asset($comic['thumb']) }}" alt="#">
+                    <h5>{{ $comic['series'] }}</h5>
                 </div>
+                @endforeach
             </div>
             <button>LOAD MORE</button>
         </div>
